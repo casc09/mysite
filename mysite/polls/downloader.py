41b8c9a0f94ad68bcb7c05a47f515ccad190f197
wuzheng <2464374403@>
2017-09-30 10:30:21 +0800

    add downloadfile.py

--- mysite/polls/downloader.py
+++ mysite/polls/downloader.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import youtube_dl
+from django.http import HttpResponse
 
+# 接收请求数据
+def search(request):
+    request.encoding = 'utf-8'
+    downloadUrl=request.GET.get('downloadUrl')
+    urls=[downloadUrl]
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(urls)
+    return HttpResponse(message)
