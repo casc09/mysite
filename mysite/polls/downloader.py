0d2dc30b73d5b10518f1744a5ff68c03ff13c087
wuzheng <2464374403@>
2017-10-31 15:56:46 +0800

    add downloadfile.py

--- mysite/polls/downloader.py
+++ mysite/polls/downloader.py
@@ -1,14 +1,61 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import youtube_dl
 from django.http import HttpResponse
+import youtube_dl
+import json
 
-# 接收请求数据
+# take the request
 def search(request):
     request.encoding = 'utf-8'
-    downloadUrl=request.GET.get('downloadUrl')
-    urls=[downloadUrl]
-    ydl_opts = {}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(urls)
-    return HttpResponse(message)
+    downloadUrl = request.GET.get('downloadUrl')
+
+    #get video urls
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+
+    with ydl:
+        result = ydl.extract_info(
+            downloadUrl,
+            # 'https://www.youtube.com/watch?v=-StEklVt4Tk',
+            # 'https://www.bilibili.com/video/av15624251/',
+            download=False  # We just want to extract the info
+        )
+
+    if 'entries' in result:
+        # Can be a playlist or a list of videos
+        video = result['entries'][0]
+    else:
+        # Just a video
+        video = result
+
+    #deal with the data returned by youtube-dl
+    # print(video)
+    audiosAndVideos = video['formats']
+    audios = []
+    videosSmall = []
+    videosMedium = []
+    videosHd720 = []
+    for mat in audiosAndVideos:
+        if mat['format_note'] == 'DASH audio':
+            audios.append(mat)
+        if mat['format_note'] == 'small':
+            videosSmall.append(mat)
+        if mat['format_note'] == 'medium':
+            videosMedium.append(mat)
+        if mat['format_note'] == 'hd720':
+            videosHd720.append(mat)
+    # response object
+    resp = {}
+    if len(audiosAndVideos) > 0 :
+        resp['success']='yes'
+    else:
+        resp['success'] = 'no'
+    resp['audios']=audios
+    resp['videosSmall']=videosSmall
+    resp['videosMedium']=videosMedium
+    resp['videoHd720']=videosHd720
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+    # video_info = videosHd720[0]
+    # video_url = video_info['url']
+    # video_formate = video_info['ext']
+    # print(video_formate)
